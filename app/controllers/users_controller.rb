7d0cf4b9fb75b61f01b6e5df684b1cffb922dd15
Paul Pukhtiy <priest1406@gmail.com>
2017-09-01 19:16:13 +0600

    Fix: adding company id to users, creating from application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
   end
 
   def new
-    @users = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @users, notice: 'User was successfully created.'
+      redirect_to @user, notice: 'User was successfully created.'
     else
       render :new
     end
