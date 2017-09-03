5991122fd6c0473402582fc6eaf94327985460d5
Paul Pukhtiy <priest1406@gmail.com>
2017-09-03 21:52:51 +0600

    New: add bootstrap and localize

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ module ContractorsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Almaty'
   end
 end
