abf6c1e66a58ddb751574e320d668450e87c028f
Paul Pukhtiy <priest1406@gmail.com>
2017-10-06 18:35:52 +0600

    New: deleting migrations

--- db/migrate/20171006102949_create_order_table_products.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderTableProducts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :order_table_products do |t|
-      t.integer :order_table_id
-      t.integer :product_id
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20171006103200_create_order_table_price_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderTablePriceLists < ActiveRecord::Migration[5.1]
-  def change
-    create_table :order_table_price_lists do |t|
-      t.integer :order_table_id
-      t.integer :price_list_id
-      t.timestamps
-    end
-  end
-end
