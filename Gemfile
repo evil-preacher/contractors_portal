bb12d38cba5c87774b226bf0f53dd16ce16dd20b
Paul Pukhtiy <priest1406@gmail.com>
2018-02-05 11:32:51 +0000

    New: add import from excel files

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'roo'
+gem 'roo-xls'
+gem 'iconv'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Product < ApplicationRecord
   belongs_to :product_group
   belongs_to :company
@@ -11,8 +13,22 @@ class Product < ApplicationRecord
   validates :barcode, presence: true, uniqueness: true, length: { maximum: 20 }
 
   def self.import(file)
-    CSV.foreach(file.path, headers:true) do |row|
-      Product.create! row.to_hash
+    spreadsheet = Roo::Spreadsheet.open(file.path)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      product = find_by(id: row["id"]) || new
+      product.attributes = row.to_hash
+      product.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then Roo::CSV.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
     end
   end
 end
