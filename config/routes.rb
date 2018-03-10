f1e5ce12c798488dbc3a3185d008a40e005e9674
Paul Pukhtiy <priest1406@gmail.com>
2018-03-10 16:30:21 +0600

    Clear routes from deleting endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   resources :price_lists, only: :index
   resources :price_types, only: :index
   resources :categories, only: :index
-  resources :products, only: [:index, :show] do
-    collection { post :import }
-  end
+  resources :products, only: [:index, :show]
   resources :brands, only: :index
   resources :load_events, only: [:index, :show, :new, :create]
   resources :remainders, only: :index
@@ -25,24 +23,16 @@ Rails.application.routes.draw do
       resources :companies, only: :show
       post 'categories/batch_create', to: 'categories#batch_create'
       post 'brands/batch_create', to: 'brands#batch_create'
-      delete 'brands/clear_all', to: 'brands#clear_all'
       get 'price_lists/index', to: 'price_lists#index'
       post 'price_lists/batch_create', to: 'price_lists#batch_create'
-      delete 'price_lists/clear_all', to: 'price_lists#clear_all'
       post 'price_types/batch_create', to: 'price_types#batch_create'
-      delete 'price_types/clear_all', to: 'price_types#clear_all'
       post 'shops/batch_create', to: 'shops#batch_create'
-      delete 'shops/clear_all', to: 'shops#clear_all'
       post 'sales_agents/batch_create', to: 'sales_agents#batch_create'
-      delete 'sales_agents/clear_all', to: 'sales_agents#clear_all'
       get 'remainders/index', to: 'remainders#index'
       post 'remainders/batch_create', to: 'remainders#batch_create'
-      delete 'remainders/clear_all', to: 'remainders#clear_all'
       post 'products/batch_create', to: 'products#batch_create'
-      delete 'products/clear_all', to: 'products#clear_all'
     end
   end
 
   root 'home#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
