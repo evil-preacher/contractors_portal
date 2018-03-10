474b22df10e819f7c367a37efec3f9cec1ee84de
Paul Pukhtiy <priest1406@gmail.com>
2018-03-10 16:31:02 +0600

    Delete product imports methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,26 +8,6 @@ class Product < ApplicationRecord
   validates :barcode, presence: true, length: { maximum: 20 }
   validates :category_accounting_system_code, presence: true
 
-  def self.import(file)
-    spreadsheet = Roo::Spreadsheet.open(file.path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      product = find_by(id: row["id"]) || new
-      product.attributes = row.to_hash
-      product.save!
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when ".csv" then Roo::CSV.new(file.path, nil, :ignore)
-    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
-    else raise "Unknown file type: #{file.original_filename}"
-    end
-  end
-
   def self.batch_create(products)
     begin
       Product.transaction do
