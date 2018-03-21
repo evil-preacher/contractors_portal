e7939352d51a22d5b3c4adb7416ff9c6647e4c37
Paul Pukhtiy <priest1406@gmail.com>
2018-03-21 14:51:27 +0600

    Add validations to geodata

--- app/models/geodatum.rb
+++ app/models/geodatum.rb
@@ -1,3 +1,8 @@
 class Geodatum < ApplicationRecord
   belongs_to :company
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :time, presence: true
+  validates :sales_agent_asc, presence: true
 end
