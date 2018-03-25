c14a4d3ee552f7556e0e31ec68bfff6eaa853247
Paul Pukhtiy <priest1406@gmail.com>
2018-03-25 17:39:10 +0600

    remove barcode validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,5 @@ class Product < ApplicationRecord
 
   validates :title, presence: true
   validates :accounting_system_code, presence: true, length: { maximum: 20 }
-  validates :barcode, presence: true, length: { maximum: 20 }
   validates :category_accounting_system_code, presence: true
 end
