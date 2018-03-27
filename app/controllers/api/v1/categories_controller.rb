2e213a6bcb83bd611f25f6049fa15739a9f075a3
Paul Pukhtiy <priest1406@gmail.com>
2018-03-27 14:53:06 +0600

    Final version for category group load method

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,19 +2,15 @@ class Api::V1::CategoriesController < Api::V1::BaseController
 
   def create
     Category.where(company_id: current_user.company.id).delete_all
-    @categories = []
     @wrong_objects = []
-    @counter = 0
     params["categories"].each do |key, value|
-      @counter += 1
       if value[:accounting_system_code] && value[:title]
         @category = current_user.company.categories.create(category_params(value))
-        @categories << @category
       else
         @wrong_objects << key
       end
     end
-    if @categories.length == @counter
+    if @wrong_objects.empty?
       render json: {success: 'Категории выгружены'}, status: :created
     else
       render json: {failed: "Категории не выгружены, некорректные данные: #{@wrong_objects}"}, status: :unprocessable_entity
@@ -25,6 +21,8 @@ class Api::V1::CategoriesController < Api::V1::BaseController
   private
 
   def category_params(my_params)
-    my_params.permit(:accounting_system_code, :title, :parent_code)
+    my_params.permit(:accounting_system_code,
+                     :title,
+                     :parent_code)
   end
 end
