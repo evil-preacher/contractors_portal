09d462dfab54cba3f2b25ef8021862e48fc20660
Paul Pukhtiy <priest1406@gmail.com>
2018-04-23 14:31:27 +0600

    Delete sales_agents model, controller and links

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,6 @@ class Company < ApplicationRecord
   has_many :shops, dependent: :restrict_with_error
   has_many :brands, dependent: :restrict_with_error
   has_many :remainders, dependent: :restrict_with_error
-  has_many :sales_agents, dependent: :restrict_with_error
   has_many :price_types, dependent: :restrict_with_error
   has_many :price_lists, dependent: :restrict_with_error
   has_many :load_events, dependent: :restrict_with_error
--- app/models/sales_agent.rb
+++
@@ -1,7 +0,0 @@
-class SalesAgent < ApplicationRecord
-  belongs_to :company
-
-  validates :name, presence: true
-  validates :asc, presence: true, length: { maximum: 20 }
-  validates :IMEI, presence: true, uniqueness: true, length: { is: 15 }
-end
