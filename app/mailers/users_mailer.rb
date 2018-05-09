4b0e1a50a5dda8580828c25966c6840872f48bb7
Paul Pukhtiy <priest1406@gmail.com>
2018-05-09 13:13:51 +0600

    Delete send mail after user create

--- app/mailers/users_mailer.rb
+++
@@ -1,6 +0,0 @@
-class UsersMailer < ApplicationMailer
-  def create_user(user)
-    @user = user
-    mail(to: user.email, subject: "На портале создан пользователь")
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,4 @@ class User < ApplicationRecord
   has_many   :routes
 
   validates :name, presence: true
-
-  after_create :send_notification_create
-
-  private
-
-  def send_notification_create
-    unless self.admin?
-      UsersMailer.create_user(self).deliver_now
-    end
-  end
 end
