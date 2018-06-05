5e24e2358ea6785c12fb0c017090184662bdb570
Paul Pukhtiy <priest1406@gmail.com>
2018-06-05 11:43:21 +0600

    Change views element

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :office, :password,
+    params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :confirmed_at,
                                  :company_id, :sales_agent)
   end
