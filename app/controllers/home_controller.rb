ec0d1f66d92c7f6863c243a21066b52d8fc26a5a
Paul Pukhtiy <priest1406@gmail.com>
2018-06-21 12:29:46 +0600

    Create dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
 
-  def show; end
+  def show
+    @categories = current_user.company.categories.count
+    @products = current_user.company.products.count
+    @shops = current_user.company.shops.count
+    @orders = current_user.company.order_headers.count
+    @orders_loaded = current_user.company.order_headers.where(loaded: true).count
+    @users = current_user.company.users.where(sales_agent: true).count
+  end
 end
